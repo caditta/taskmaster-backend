cecfe9c1b9ce66e9c7f13b2dc5e90d597c66d408
Caditta <caditta@gmail.com>
2024-10-20 17:33:06 -0500

    Creacion de archifos bd.js y demas carpetas de raiz

--- src/app.js
+++ src/app.js
@@ -1,19 +1,16 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const cors = require('cors');
-
-dotenv.config();
 const app = express();
-
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send("El servidor esta corriendo");
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando');
 });
 
-app.listen(port,()=>{
-    console.log("port ==>", port);
+// Puerto
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
