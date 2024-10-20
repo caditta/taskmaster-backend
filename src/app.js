5f67e5f3e61f97a4ae0ab6ddca5e280a8b5c92ef
Caditta <caditta@gmail.com>
2024-10-20 17:02:44 -0500

    Build inicial

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send("El servidor esta corriendo");
+});
+
+app.listen(port,()=>{
+    console.log("port ==>", port);
+});
